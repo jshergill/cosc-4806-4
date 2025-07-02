f0aa59a6eb587eddeb3e154d54c25e6cddcc576f
jshergill <jshergill@algomau.ca>
2025-07-02 19:27:14 +0000

    added reminders home page

---
+++ app/controllers/reminders.php
@@ -0,0 +1,11 @@
+<?php
+
+class Reminders extends Controller {
+
+    public function index() {
+        $reminder = $this->model('Reminder');
+        $list_of_reminders = $reminder->get_all_reminders();
+        $this->view('reminders/index', ['reminders' => $list_of_reminders]);
+    }
+    
+}
