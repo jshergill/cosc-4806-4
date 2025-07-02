78845edb2febc2dd8ab13f2fe3e93619998a254c
jshergill <jshergill@algomau.ca>
2025-07-02 19:35:15 +0000

    Fetching all the reminders

---
+++ app/models/Reminder.php
@@ -0,0 +1,15 @@
+<?php
+
+class Reminder {
+
+    public function __construct() {}
+
+
+    public function get_all_reminders() {
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * FROM reminders;");
+        $statement->execute();
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+    }
+}
